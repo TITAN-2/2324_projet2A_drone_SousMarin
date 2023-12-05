255df813b8d4f22a481ac200e899206caabe8ea3
PiThorez <pierre.thorez@ensea.fr>
2023-12-05 17:21:42 +0100

    update joysticks

--- Web/Control/Control.js
+++ Web/Control/Control.js
@@ -1,48 +1,97 @@
-var joystickHandle = document.getElementById('joystick-vitesse-handle');
-var container = document.getElementById('joystick-vitesse-container');
-var isDragging = false;
+var joystickVitesseHandle = document.getElementById('joystick-vitesse-handle');
+var joystickVitesseContainer = document.getElementById('joystick-vitesse-container');
+var isDraggingVitesse = false;
 
-joystickHandle.addEventListener('mousedown', function (e) {
-    isDragging = true;
-    container.style.transition = 'none';
-    updateJoystick(e);
+var joystickProfondeurHandle = document.getElementById('joystick-profondeur-handle');
+var joystickProfondeurContainer = document.getElementById('joystick-profondeur-container');
+var isDraggingProfondeur = false;
+
+var joystickDirectionHandle = document.getElementById('joystick-direction-handle');
+var joystickDirectionContainer = document.getElementById('joystick-direction-container');
+var isDraggingDirection = false;
+
+// Event listeners for Joystick Vitesse
+joystickVitesseHandle.addEventListener('mousedown', function (e) {
+    isDraggingVitesse = true;
+    joystickVitesseContainer.style.transition = 'none';
+    updateJoystickVitesse(e, joystickVitesseHandle, joystickVitesseContainer, isDraggingVitesse);
+});
+
+// Event listeners for Joystick Profondeur
+joystickProfondeurHandle.addEventListener('mousedown', function (e) {
+    isDraggingProfondeur = true;
+    joystickProfondeurContainer.style.transition = 'none';
+    updateJoystickProfondeur(e, joystickProfondeurHandle, joystickProfondeurContainer, isDraggingProfondeur);
+});
+
+// Event listeners for Joystick Direction
+joystickDirectionHandle.addEventListener('mousedown', function (e) {
+    isDraggingDirection = true;
+    joystickDirectionContainer.style.transition = 'none';
+    updateJoystickDirection(e, joystickDirectionHandle, joystickDirectionContainer, isDraggingDirection);
 });
 
 document.addEventListener('mousemove', function (e) {
-    if (isDragging) {
-        updateJoystick(e);
+    if (isDraggingVitesse) {
+        updateJoystickVitesse(e, joystickVitesseHandle, joystickVitesseContainer, isDraggingVitesse);
+    }
+    if (isDraggingProfondeur) {
+        updateJoystickProfondeur(e, joystickProfondeurHandle, joystickProfondeurContainer, isDraggingProfondeur);
+    }
+    if (isDraggingDirection) {
+        updateJoystickDirection(e, joystickDirectionHandle, joystickDirectionContainer, isDraggingDirection);
     }
 });
 
 document.addEventListener('mouseup', function () {
-    if (isDragging) {
-        isDragging = false;
-        container.style.transition = 'all 0.3s ease';
-        resetJoystick();
+    if (isDraggingVitesse) {
+        isDraggingVitesse = false;
+        joystickVitesseContainer.style.transition = 'all 0.3s ease';
+        resetJoystick(joystickVitesseHandle);
+    }
+    if (isDraggingProfondeur) {
+        isDraggingProfondeur = false;
+        joystickProfondeurContainer.style.transition = 'all 0.3s ease';
+        resetJoystick(joystickProfondeurHandle);
+    }
+    if (isDraggingDirection) {
+        isDraggingDirection = false;
+        joystickDirectionContainer.style.transition = 'all 0.3s ease';
+        resetJoystick(joystickDirectionHandle);
     }
 });
 
-function updateJoystick(e) {
-    var containerRect = container.getBoundingClientRect();
+function updateJoystickVitesse(e) {
+    var containerRect = joystickVitesseContainer.getBoundingClientRect();
     var y = e.clientY - containerRect.top - containerRect.height / 2;
 
-    var distance = containerRect.height / 2; // Diviser par 2 pour rester dans la plage de -distance à distance
-    var offsetY = Math.min(Math.max(y, -1.80*distance), -distance/4.8); // Limiter la position verticale
+    var distance = containerRect.height / 2;
+    var offsetY = Math.min(Math.max(y, -1.80 * distance), -distance / 4.8);
 
-    joystickHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
+    joystickVitesseHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
 }
 
-
-function updateJoystick1(e) {
-    var containerRect = container.getBoundingClientRect();
-    var y = e.clientY - containerRect.top - containerRect.height/2;
+function updateJoystickProfondeur(e) {
+    var containerRect = joystickProfondeurContainer.getBoundingClientRect();
+    var y = e.clientY - containerRect.top - containerRect.height / 2;
 
     var distance = containerRect.height / 2;
-    var offsetY = Math.min(Math.max(y, -distance), distance/1.68); // Limiter la position verticale
+    var offsetY = Math.min(Math.max(y, -distance), distance / 1.68);
 
-    joystickHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
+    joystickProfondeurHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
 }
 
-function resetJoystick() {
-    joystickHandle.style.transform = 'translate(-50%, -50%)';
+function updateJoystickDirection(e) {
+    var containerRect = joystickDirectionContainer.getBoundingClientRect();
+    var x = e.clientX - containerRect.left - containerRect.width / 2;
+
+    var distance = containerRect.width / 2;
+    var offsetX = Math.min(Math.max(x, -distance), distance/1.34);
+
+    joystickDirectionHandle.style.transform = 'translate(' + offsetX + 'px, -50%)';
 }
+
+function resetJoystick(handle) {
+    handle.style.transform = 'translate(-50%, -50%)';
+}
+
