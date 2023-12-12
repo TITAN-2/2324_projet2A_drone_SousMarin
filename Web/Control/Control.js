ccfe679a0fb496d1d87f14b0c4ba0045df87cfa5
PiThorez <pierre.thorez@ensea.fr>
2023-12-12 16:01:02 +0100

    updates Controls

--- Web/Control/Control.js
+++ Web/Control/Control.js
@@ -10,6 +10,10 @@ var joystickDirectionHandle = document.getElementById('joystick-direction-handle
 var joystickDirectionContainer = document.getElementById('joystick-direction-container');
 var isDraggingDirection = false;
 
+resetVitesseValue();
+resetProfondeurValue();
+resetDirectionValue();
+
 // Event listeners for Joystick Vitesse
 joystickVitesseHandle.addEventListener('mousedown', function (e) {
     isDraggingVitesse = true;
@@ -48,50 +52,111 @@ document.addEventListener('mouseup', function () {
         isDraggingVitesse = false;
         joystickVitesseContainer.style.transition = 'all 0.3s ease';
         resetJoystick(joystickVitesseHandle);
+        resetVitesseValue();
     }
     if (isDraggingProfondeur) {
         isDraggingProfondeur = false;
         joystickProfondeurContainer.style.transition = 'all 0.3s ease';
         resetJoystick(joystickProfondeurHandle);
+        resetProfondeurValue();
     }
     if (isDraggingDirection) {
         isDraggingDirection = false;
         joystickDirectionContainer.style.transition = 'all 0.3s ease';
         resetJoystick(joystickDirectionHandle);
+        resetDirectionValue();
     }
 });
 
 function updateJoystickVitesse(e) {
     var containerRect = joystickVitesseContainer.getBoundingClientRect();
-    var y = e.clientY - containerRect.top - containerRect.height / 2;
+    var y = e.clientY - containerRect.top - containerRect.height;
 
-    var distance = containerRect.height / 2;
+    var distance = containerRect.height /2;
     var offsetY = Math.min(Math.max(y, -1.80 * distance), -distance / 4.8);
 
     joystickVitesseHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
+
+    var vitesseValue = calculateJoystickValueVitesse(joystickVitesseContainer, joystickVitesseHandle);
+    document.getElementById('vitesse-value').innerText = vitesseValue.toFixed(1); // Ajustez le formatage selon vos besoins
 }
 
 function updateJoystickProfondeur(e) {
     var containerRect = joystickProfondeurContainer.getBoundingClientRect();
-    var y = e.clientY - containerRect.top - containerRect.height / 2;
+    var y = e.clientY - containerRect.top - containerRect.height / 2  -25;
 
     var distance = containerRect.height / 2;
     var offsetY = Math.min(Math.max(y, -distance), distance / 1.68);
 
     joystickProfondeurHandle.style.transform = 'translate(-50%, ' + offsetY + 'px)';
+
+    var profondeurValue = calculateJoystickValueProfondeur(joystickProfondeurContainer, joystickProfondeurHandle);
+    document.getElementById('profondeur-value').innerText = profondeurValue.toFixed(1); // Ajustez le formatage selon vos besoins
 }
 
 function updateJoystickDirection(e) {
     var containerRect = joystickDirectionContainer.getBoundingClientRect();
-    var x = e.clientX - containerRect.left - containerRect.width / 2;
+    var x = e.clientX - containerRect.left - containerRect.width /2  - 25;
 
     var distance = containerRect.width / 2;
-    var offsetX = Math.min(Math.max(x, -distance), distance/1.34);
+    var offsetX = Math.min(Math.max(x, -distance), distance/1.3515);
 
     joystickDirectionHandle.style.transform = 'translate(' + offsetX + 'px, -50%)';
+
+    var directionValue = calculateJoystickValueDirection(joystickDirectionContainer, joystickDirectionHandle);
+    document.getElementById('direction-value').innerText = directionValue.toFixed(1); // Ajustez le formatage selon vos besoins
 }
 
 function resetJoystick(handle) {
     handle.style.transform = 'translate(-50%, -50%)';
 }
 
+
+
+function calculateJoystickValueProfondeur(container, handle) {
+    // Calculer la valeur relative de la position de la poignée dans le conteneur
+    var containerRect = container.getBoundingClientRect();
+    var handleRect = handle.getBoundingClientRect();
+
+    var value;
+
+    value = (handleRect.top - containerRect.top) / containerRect.height;
+    value = (0.8-value)*100/0.8 -50;
+    return value;
+}
+
+function calculateJoystickValueVitesse(container, handle) {
+    // Calculer la valeur relative de la position de la poignée dans le conteneur
+    var containerRect = container.getBoundingClientRect();
+    var handleRect = handle.getBoundingClientRect();
+
+    var value;
+
+    value = (handleRect.top - containerRect.top) / containerRect.height;
+    value = (0.8-value)*100/0.8;
+    return value;
+}
+
+function calculateJoystickValueDirection(container, handle) {
+    // Calculer la valeur relative de la position de la poignée dans le conteneur
+    var containerRect = container.getBoundingClientRect();
+    var handleRect = handle.getBoundingClientRect();
+
+    var value;
+    value = (handleRect.left - containerRect.left) / containerRect.width;
+    value = value*100/0.87 -50;
+    return value;
+}
+
+
+function resetVitesseValue() {
+    document.getElementById('vitesse-value').innerText = '0.0'; // Mettez la valeur par défaut selon vos besoins
+}
+
+function resetProfondeurValue() {
+    document.getElementById('profondeur-value').innerText = '0.0'; // Mettez la valeur par défaut selon vos besoins
+}
+
+function resetDirectionValue() {
+    document.getElementById('direction-value').innerText = '0.0'; // Mettez la valeur par défaut selon vos besoins
+}
