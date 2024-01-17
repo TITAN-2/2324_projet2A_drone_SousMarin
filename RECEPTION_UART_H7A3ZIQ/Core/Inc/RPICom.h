d91f3cac0e53b8773b30a51b3f6ddaa90d2b986f
romingodelfuego <arbrefrancais@gmail.com>
2024-01-17 16:27:36 +0100

    RPICom

--- RECEPTION_UART_H7A3ZIQ/Core/Inc/RPICom.h
+++ RECEPTION_UART_H7A3ZIQ/Core/Inc/RPICom.h
@@ -8,13 +8,17 @@
 #ifndef INC_RPICOM_H_
 #define INC_RPICOM_H_
 
-#define UART_RPI_RX_BUFFER_SIZE 5
+//#define UART_RX huart4
+//#define UART_TX
+//#define UART_TERMINAL huart3
+#define UART_RPI_RX_BUFFER_SIZE 8
+#define HEADER_CheckValue1 0xFF
+#define HEADER_CheckValue2 0xFE
 #include "usart.h"
 
 
-
-
 typedef struct {
+
     uint8_t thrust;
     int8_t angle;
     uint16_t depth;
--- RECEPTION_UART_H7A3ZIQ/Core/Src/RPICom.c
+++ RECEPTION_UART_H7A3ZIQ/Core/Src/RPICom.c
@@ -11,21 +11,40 @@ void RPICom_UartActivate(void){
 	HAL_UART_Receive_IT(&huart4, uartRpiRxBuffer, UART_RPI_RX_BUFFER_SIZE);
 }
 
+uint8_t checkSum(void){
+	uint8_t checkSum = 0;
+	for (int i = 0; i < UART_RPI_RX_BUFFER_SIZE-1; i++) { //Il ne faut pas prendre en compte le bit ou checkSum est present
+	        checkSum ^= uartRpiRxBuffer[i];
+	    }
+	return checkSum;
+}
 
 void RPICom_DecodeBinaryMessage(void)
 {
-	// A RAJOUTER --> Check header
-	// A RAJOUTER --> Length
-	binaryMessage.thrust = uartRpiRxBuffer[0];
-	binaryMessage.angle = uartRpiRxBuffer[1];
-	binaryMessage.depth = ((uint16_t)uartRpiRxBuffer[2]+(uint16_t)(uartRpiRxBuffer[3]<<8));
-	binaryMessage.paquetNumber = uartRpiRxBuffer[4];
-	// A RAJOUTER --> Checksum
-	// A RAJOUTER --> Réponse à la RPI
-	HAL_UART_Transmit(&huart3, &binaryMessage, 5, 10);
+	char error = '0';
+	// Check Header
+	if (  HEADER_CheckValue1 != uartRpiRxBuffer[0] || HEADER_CheckValue2 != uartRpiRxBuffer[1]){//On envoie une erreur à la raspberry
+		error ='1';}
+	// Check Length
+	if (UART_RPI_RX_BUFFER_SIZE != uartRpiRxBuffer[2]){// Il nous manque des informations
+	error='2';
+	}
 
-	// HAL_UART_Transmit(&huart3,&recvd_data,sizeof(recvd_data));
-	// HAL_UART_Transmit(&huart3,&binaryMessage,sizeof(binaryMessage));
+	binaryMessage.thrust = uartRpiRxBuffer[3];
+	binaryMessage.angle = uartRpiRxBuffer[4];
+	binaryMessage.depth = ((uint16_t)uartRpiRxBuffer[5]+(uint16_t)(uartRpiRxBuffer[6]<<8));
+	binaryMessage.paquetNumber = uartRpiRxBuffer[7];
+
+	//Checksum
+	if (checkSum() !=  uartRpiRxBuffer[8]){
+		error='3';
+	}
+
+	//Response RPI
+	char RPI_Response[50];
+	sprintf(RPI_Response, "Paquet: %u\r\nStatut: %c\r\n", binaryMessage.paquetNumber,error);
+	HAL_UART_Transmit(&huart3, (uint8_t*)RPI_Response, sizeof(RPI_Response), 10);
+	RPICom_UartActivate();
 
 	// Vous avez maintenant un message complet dans la structure BinaryMessage
 	// Vous pouvez faire quelque chose avec ces données, par exemple, appeler une fonction de traitement
@@ -39,7 +58,7 @@ void RPICom_DecodeBinaryMessage(void)
 //HAL_UART_Receive_IT(&huart4,&recvd_data,sizeof(recvd_data));
 
 
-/*void Process(BinaryMessage* message){
+/*void Process(commandMsg* message){
 	AdjustThrust(message->thrust);
 	AdjustAngle(message->angle);
 
